ce613c047b8be03717848f5327e248e57926cc46
wbsxhh201771 <3125260538@qq.com>
2025-01-15 13:48:23 +0800

    gengai

--- Exchangeapp_backend/main.go
+++ Exchangeapp_backend/main.go
@@ -1,52 +1,13 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
-
-	"exchangeapp/config"
-	"exchangeapp/router"
-)
-
+import "github.com/gin-gonic/gin"
 
 func main() {
- config.InitConfig()
-
- r := router.SetupRouter()
-
-
-
-port := config.AppConfig.App.Port
-
-	if port ==""{
-		port = ":8080"
-	}
-
-	srv := &http.Server{
-		Addr:    port,
-		Handler: r,
-	   }
-	   
-		go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("listen: %s\n", err)
-	   }
-	   }()
-	   
-	   quit := make(chan os.Signal, 1)
-	   signal.Notify(quit, os.Interrupt)
-	   <-quit
-	   log.Println("Shutdown Server ...")
-	   
-	   ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	   defer cancel()
-	   if err := srv.Shutdown(ctx); err != nil {
-	   log.Fatal("Server Shutdown:", err)
-	   }
-	   log.Println("Server exiting")
-   
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
